e6b3d3bd3d97048ad6b4b95c22c8e4b48878266d
Aleksandr Bazhin <alexander.bazhin@mail.ru>
2021-11-30 21:56:10 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Упражнение_6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string wordWithMaxLength = string.Empty;
+            string[] words = s.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+                if (words[i].Length > wordWithMaxLength.Length)
+                    wordWithMaxLength = words[i];
+            Console.WriteLine(wordWithMaxLength);
+
+            Console.WriteLine();
+            Console.Write("Для завершения нажмите любую клавишу на клавиатуре");
+            Console.ReadKey();
+        }
+    }
+}
